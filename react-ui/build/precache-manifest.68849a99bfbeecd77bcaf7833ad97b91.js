d648e69083e86e9b1aab7b27e8560dbb5818e971
Siva Karthik <siramesh@akamai.com>
2020-03-18 18:02:43 +0530

    Initial Commit

---
+++ react-ui/build/precache-manifest.68849a99bfbeecd77bcaf7833ad97b91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41bac32a7e7d708c4827d30b8d04904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbcf0e5d34bad8ccf8ff",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "056ce274c7f69ed492db",
+    "url": "/static/js/2.7fb07539.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7fb07539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbcf0e5d34bad8ccf8ff",
+    "url": "/static/js/main.96c44a22.chunk.js"
+  },
+  {
+    "revision": "07be4d0a59389f43a71c",
+    "url": "/static/js/runtime-main.5e59705e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
